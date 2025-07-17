6abd5ffca947f8e0a06c6223ffdd40e7aff417cd
Kakuren <klong8257@gmail.com>
2025-07-17 10:52:31 +0700

    add PID

--- lidarrobot.py
+++ lidarrobot.py
@@ -1,6 +1,5 @@
 from robomaster import robot
 
-
 if __name__ == '__main__':
     ep_robot = robot.Robot()
     ep_robot.initialize(conn_type="ap")
--- myrobot/Scripts/activate.bat
+++ myrobot/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\asus\Desktop\Lab01x\myrobot
+set VIRTUAL_ENV=C:\Users\asus\Documents\GitHub\Robotmaster\myrobot
 
 if not defined PROMPT set PROMPT=$P$G
 
